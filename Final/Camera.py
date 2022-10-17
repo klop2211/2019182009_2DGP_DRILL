43e504697a5ca4a4c1810f6132a36d8928e74911
klop2211 <58390003+klop2211@users.noreply.github.com>
2022-10-17 16:31:44 +0900

    카메라 초기값 및 이동 함수 수정

--- Final/Camera.py
+++ Final/Camera.py
@@ -1,16 +1,16 @@
 class Camera:
     def __init__(self):
-        self.x = 400
-        self.y = 400
+        self.x = 0
+        self.y = 0
 
     def move(self, dx, dy):
         self.x -= dx
-        if self.x <= 300:
-            self.x = 300
-        elif self.x >= 500:
-            self.x = 500
+        if self.x <= -200:
+            self.x = -200
+        elif self.x >= 0:
+            self.x = 0
         self.y -= dy
-        if self.y <= 200:
-            self.y = 200
-        elif self.y >= 400:
-            self.y = 400
+        if self.y <= -200:
+            self.y = -200
+        elif self.y >= 0:
+            self.y = 0
