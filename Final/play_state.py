3d961a46398a0a4d26087fd72e9f884e7d8294d6
klop2211 <58390003+klop2211@users.noreply.github.com>
2022-10-18 00:00:50 +0900

    미니맵 추가 및 맵 이동버튼 추가

--- Final/play_state.py
+++ Final/play_state.py
@@ -1,22 +1,27 @@
 import game_framework
 import Camera
 import Map_object
+import UI_object
 from pico2d import *
 
 camera = None
 map = None
+minimap = None
 
 def enter():
-    global map, camera
+    global map, camera, minimap
     map = Map_object.Map()
     camera = Camera.Camera()
+    minimap = UI_object.Minimap()
 
 def update():
     pass
 
 def draw():
+    global map, camera, minimap
     clear_canvas()
     map.draw(camera.x, camera.y)
+    minimap.draw(map.map_num)
     update_canvas()
     pass
 
@@ -27,6 +32,7 @@ def handle_events():
             game_framework.quit()
         elif event.type == SDL_KEYDOWN:
             match event.key:
+                # 카메라 이동을 위한 임시 문장
                 case pico2d.SDLK_UP:
                     camera.move(0, 50)
                 case pico2d.SDLK_DOWN:
@@ -35,6 +41,16 @@ def handle_events():
                     camera.move(-50, 0)
                 case pico2d.SDLK_RIGHT:
                     camera.move(50, 0)
+                # 맵 이동을 위한 임시 문장
+                case pico2d.SDLK_1:
+                    map.map_num = 0
+                case pico2d.SDLK_2:
+                    map.map_num = 1
+                case pico2d.SDLK_3:
+                    map.map_num = 2
+                case pico2d.SDLK_4:
+                    map.map_num = 3
+
 
 
 def exit():
